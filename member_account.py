0d64ee6cb07fc6abb0576aed2e16f1671d858228
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-02-04 10:59:01 +0100

    exercise7

---
+++ member_account.py
@@ -0,0 +1,69 @@
+from state_machine import State, Transition, Transitions, StateMachine
+
+
+class MemberAccount:
+    """MemberAccount represents an account of a club member.
+
+    After the member registers, the account must be confirmed to be active. When active, account information can be
+    changed by the member. A member can suspend her account and reactivate it. If the annual fee is due, the account
+    will be set inactive until the fee is paid by the member. A member can cancel the account to delete it.
+
+    All methods change the state of the account, but only valid transition are possible. If an invalid transition
+    is requested, methods will raise a RuntimeError. See _transitions for all valid transitions.
+    """
+
+    START = State.START
+    REGISTERED = State("registered")
+    ACTIVE = State("active")
+    INACTIVE = State("inactive")
+    DORMANT = State("dormant")
+    END = State.END
+
+    _transitions = Transitions(Transition(START, "register", REGISTERED),
+                               Transition(REGISTERED, "confirm", ACTIVE),
+                               Transition(ACTIVE, "change", ACTIVE),
+                               Transition(ACTIVE, "suspend", DORMANT),
+                               Transition(ACTIVE, "fee due", INACTIVE),
+                               Transition(ACTIVE, "cancel", END),
+                               Transition(DORMANT, "reactivate", ACTIVE),
+                               Transition(INACTIVE, "transfer", ACTIVE))
+
+    def __init__(self):
+        """Initializes the account to State START."""
+        self.machine = StateMachine(MemberAccount._transitions)
+
+    def get_state(self):
+        """Returns the current state of the account."""
+        return self.machine.state
+
+    def register(self):
+        """Sets the state of the account to REGISTERED after a new member registered."""
+        self.machine.transition(self.REGISTERED)
+
+    def confirm(self):
+        """Sets the state of the account to ACTIVE after the new member confirmed her mail address."""
+        self.machine.transition(self.ACTIVE)
+
+    def change(self):
+        """Updates member information and sets the state of the account to ACTIVE."""
+        self.machine.transition(self.ACTIVE)
+
+    def suspend(self):
+        """Sets the state of the account to DORMANT after the member suspended the account."""
+        self.machine.transition(self.DORMANT)
+    
+    def reactivate(self):
+        """Sets the state of the account back to ACTIVE after the member reactivated her account."""
+        self.machine.transition(self.ACTIVE)
+
+    def fee_due(self):
+        """Sets the state of the account to INACTIVE because the annual fee is due."""
+        self.machine.transition(self.INACTIVE)
+
+    def transfer(self):
+        """Sets the state of the account back to ACTIVE after the member paid the annual fee."""
+        self.machine.transition(self.ACTIVE)
+
+    def cancel(self):
+        """Cancels the member account and sets the state of the account back to END."""
+        self.machine.transition(self.END)
---
+++ test_member_accont.py
@@ -0,0 +1,87 @@
+from member_account import MemberAccount
+import pytest
+
+
+# Arrange
+@pytest.fixture()
+def confirmed_account():
+    account = MemberAccount()
+    account.register()
+    account.confirm()
+    return account
+
+
+def test_member_account_start_state():
+    account = MemberAccount()
+    assert account.get_state() == MemberAccount.START
+
+
+def test_fail_when_cancelling_in_registered_state():
+    # Arrange
+    account = MemberAccount()
+    account.register()
+
+    # Act
+    with pytest.raises(RuntimeError):
+        account.cancel()
+
+
+def test_fail_when_cancelling_in_start_state():
+    # Arrange
+    account = MemberAccount()
+
+    # Act
+    with pytest.raises(RuntimeError):
+        account.cancel()
+
+
+def test_member_account_register():
+    account = MemberAccount()
+    account.register()
+
+    assert account.get_state() == MemberAccount.REGISTERED
+
+
+def test_member_account_active(confirmed_account):
+
+    assert confirmed_account.get_state() == MemberAccount.ACTIVE
+
+
+def test_creation_and_cancellation(confirmed_account):
+    # Act
+    confirmed_account.cancel()
+
+    # Assert
+    assert confirmed_account.get_state() == MemberAccount.END
+
+
+def test_creation_and_change(confirmed_account):
+    # Act
+    confirmed_account.change()
+
+    # Assert
+    assert confirmed_account.get_state() == MemberAccount.ACTIVE
+
+
+def test_member_account_feedue(confirmed_account):
+    confirmed_account.fee_due()
+
+    assert confirmed_account.get_state() == MemberAccount.INACTIVE
+
+
+def test_member_account_suspend(confirmed_account):
+    confirmed_account.suspend()
+
+    assert confirmed_account.get_state() == MemberAccount.DORMANT
+
+
+def test_member_account_transfer_of_inactive(confirmed_account):
+    confirmed_account.fee_due()
+    confirmed_account.transfer()
+    assert confirmed_account.get_state() == MemberAccount.ACTIVE
+
+
+def test_member_account_dormant_reactivate(confirmed_account):
+
+    confirmed_account.reactivate()
+    assert confirmed_account.get_state() == MemberAccount.ACTIVE
